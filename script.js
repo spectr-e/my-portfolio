9a7a96f4424d733647275d437a188215d62b2968
josiah <devs.josia@gmail.com>
2022-11-20 22:19:19 +0300

    Create intial HTML, CSS & JS content

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+const div = document.createElement('div')
+div.className = "header_wrapper"
+document.querySelector("header").appendChild(div)
+
 const h1 = document.createElement("h1");
 h1.textContent = "Website by Josiah Nganga";
-document.querySelector("main").appendChild(h1);
+document.querySelector(".header_wrapper").appendChild(h1);
 
 const h2 = document.createElement("h2");
 h2.textContent = "Welcome, feel at home.";
-document.querySelector("main").appendChild(h2);
+document.querySelector(".header_wrapper").appendChild(h2);
 
 const p = document.createElement("p");
 p.textContent = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto officia sint ipsum? Alias, voluptas magni. Illo reprehenderit aut eum molestias.Libero deleniti accusantium consectetur iste nesciunt facilis, soluta neque qui tenetur impedit sed eum omnis voluptates, asperiores praesentium voluptate et natus ipsa atque. Delectus, iusto.`;
-document.querySelector("main").appendChild(p);
+document.querySelector(".header_wrapper").appendChild(p);
